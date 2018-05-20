d1b943a369e3763193c64e4c2dd2c578cb2d3998
Luke Varley <luke.varley@auger.co.uk>
2018-05-20 20:22:10 +0100

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿//// Write your Javascript code.
+//var sendFrom = document.getElementById("send-form");
+//var sendButton = document.getElementById("send-button");
+//var messagesList = document.getElementById("messages-list");
+//var messageTextBox = document.getElementById("message-textbox");
+
+//function appendMessage(content) {
+//    var li = document.createElement("li");
+//    li.innerText = content;
+//    messagesList.appendChild(li);
+//}
+
+//var connection = new signalR.HubConnection("/hubs/chat");
+
+//sendFrom.addEventListener("submit", function (evt) {
+//    var message = messageTextBox.value;
+//    messageTextBox.value = "";
+//    connection.send("Send",message);
+//    evt.preventDefault();
+//});
+
+//connection.on("SendMessage", function (sender, message) {
+//    appendMessage(`${sender}: ${message}`)
+//});
+
+//connection.on("SendAction", function (sender, action) {
+//    appendMessage(`${sender} ${action}`);
+//});
+
+//connection.start().then(function (params) {
+//    messageTextBox.disabled = false;
+//    sendButton.disabled = false;
+//});
